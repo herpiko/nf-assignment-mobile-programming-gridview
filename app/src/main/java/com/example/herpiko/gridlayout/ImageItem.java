bab8dc211f6f1bfd9fc5aac3465bc46c279e8e72
Herpiko Dwi Aguno <herpiko@gmail.com>
2015-10-09 21:28:07 +0700

    First commit

---
+++ app/src/main/java/com/example/herpiko/gridlayout/ImageItem.java
@@ -0,0 +1,33 @@
+package com.example.herpiko.gridlayout;
+
+/**
+ * Created by herpiko on 10/9/15.
+ */
+import android.graphics.Bitmap;
+
+public class ImageItem {
+    private Bitmap image;
+    private String title;
+
+    public ImageItem(Bitmap image, String title) {
+        super();
+        this.image = image;
+        this.title = title;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
